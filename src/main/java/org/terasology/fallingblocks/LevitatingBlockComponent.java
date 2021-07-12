fc02bfb02ad159ac469d75159723ecee757d42df
Michael Pollind <mpollind@gmail.com>
2021-07-11 19:10:14 -0700

    feature(ecs-gestalt): Migrate Components to gestalt's Components ref: https://github.com/MovingBlocks/Terasology/pull/4753

--- src/main/java/org/terasology/fallingblocks/LevitatingBlockComponent.java
+++ src/main/java/org/terasology/fallingblocks/LevitatingBlockComponent.java
@@ -6,6 +6,13 @@ package org.terasology.fallingblocks;
 import org.terasology.gestalt.entitysystem.component.Component;
 
 public class LevitatingBlockComponent implements Component<LevitatingBlockComponent> {
-    /** The total mass of blocks this can support, or 0 for unlimited. */
+    /**
+     * The total mass of blocks this can support, or 0 for unlimited.
+     */
     public float strength = 0;
+
+    @Override
+    public void copy(LevitatingBlockComponent other) {
+        this.strength = other.strength;
+    }
 }
