ca351755ff5b80dc3a22a5d93e7d97512d554ac8
Nail Khanipov <foxflameless@gmail.com>
2021-08-25 16:23:21 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components. (#11)
    Ref: MovingBlocks/Terasology#4753
    
    Co-authored-by: Michael Pollind <mpollind@gmail.com>
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/fallingblocks/LevitatingBlockComponent.java
+++ src/main/java/org/terasology/fallingblocks/LevitatingBlockComponent.java
@@ -1,11 +1,18 @@
-// Copyright 2020 The Terasology Foundation
+// Copyright 2021 The Terasology Foundation
 // SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.fallingblocks;
 
-import org.terasology.engine.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.Component;
 
-public class LevitatingBlockComponent implements Component {
-    /** The total mass of blocks this can support, or 0 for unlimited. */
+public class LevitatingBlockComponent implements Component<LevitatingBlockComponent> {
+    /**
+     * The total mass of blocks this can support, or 0 for unlimited.
+     */
     public float strength = 0;
+
+    @Override
+    public void copyFrom(LevitatingBlockComponent other) {
+        this.strength = other.strength;
+    }
 }
